11c17aba168f5729f947a1f8454fb8992c543395
DayDream-21 <vova13158@gmail.com>
2021-08-09 19:22:02 +0300

    Added new bean "myPerson" and changed Test3

--- src/spring_introduction/Test3.java
+++ src/spring_introduction/Test3.java
@@ -6,8 +6,9 @@ public class Test3 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("application_context.xml");
-        Pet pet = context.getBean("myPet", Pet.class);
-        Person person = new Person(pet);
+
+        Person person = context.getBean("myPerson", Person.class);
+
         person.callYourPet();
 
         context.close();
