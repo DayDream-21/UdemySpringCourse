bb3d6b1dc474bf050f41f89426edcb106212e879
DayDream-21 <vova13158@gmail.com>
2021-08-12 17:54:52 +0300

    Configuration of project with java code, using class MyConfig with @ComponentScan annotation

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -1,30 +1,12 @@
 package spring_introduction;
 
+import org.springframework.stereotype.Component;
+
+@Component("dogBean")
 public class Dog implements Pet{
-    private String name;
-
-    public Dog() {
-        System.out.println("Dog bean is created");
-    }
-
     @Override
     public void say() {
         System.out.println("Bow-Wow");
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void init() {
-        System.out.println("Class Dog: Init method");
-    }
-
-    public void destroy() {
-        System.out.println("Class Dog: Destroy method");
-    }
 }
