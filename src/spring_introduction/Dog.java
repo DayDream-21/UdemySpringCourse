37d45f4f785d6b4fb050b8765344e71d6a535584
DayDream-21 <vova13158@gmail.com>
2021-08-12 20:06:37 +0300

    Base for new AOP branch

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -1,30 +1,8 @@
 package spring_introduction;
 
 public class Dog implements Pet{
-    private String name;
-
-    public Dog() {
-        System.out.println("Dog bean is created");
-    }
-
     @Override
     public void say() {
         System.out.println("Bow-Wow");
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void init() {
-        System.out.println("Class Dog: Init method");
-    }
-
-    public void destroy() {
-        System.out.println("Class Dog: Destroy method");
     }
 }
