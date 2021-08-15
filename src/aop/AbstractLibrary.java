71f22d833042d4b6226cac43cea1348fd493e958
DayDream-21 <vova13158@gmail.com>
2021-08-15 20:21:26 +0300

    Combining pointcut expressions

--- src/aop/AbstractLibrary.java
+++ src/aop/AbstractLibrary.java
@@ -12,7 +12,23 @@ class TownLibrary extends AbstractLibrary {
         System.out.println("We take book from Town Library.");
     }
 
+    public void returnBook() {
+        System.out.println("We return book to Town Library");
+    }
+
     public void getMagazine() {
         System.out.println("We take magazine from Town Library.");
+    }
+
+    public void returnMagazine() {
+        System.out.println("We return magazine to Town Library.");
+    }
+
+    public void addBook() {
+        System.out.println("We add book to Town Library");
+    }
+
+    public void addMagazine() {
+        System.out.println("We add magazine to Town Library");
     }
 }
