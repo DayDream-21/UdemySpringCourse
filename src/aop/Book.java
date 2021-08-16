cbee25fc3714cd76c1b98c1009333670f17fc51e
DayDream-21 <vova13158@gmail.com>
2021-08-17 01:30:25 +0300

    JoinPoint test

--- src/aop/Book.java
+++ src/aop/Book.java
@@ -8,7 +8,21 @@ public class Book {
     @Value("Amogus")
     private String name;
 
+    @Value("Abobus")
+    private String author;
+
+    @Value("1337")
+    private int year;
+
     public String getName() {
         return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
     }
 }
